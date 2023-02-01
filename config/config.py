bc716fb2ad92a669fdba6ea3f3c674e8a8d737a7
Abhijeet <abhijeet.prakash@ipl.edu.in>
2023-02-01 15:31:13 +0530

    first commit

--- config/config.py
+++ config/config.py
@@ -10,6 +10,6 @@ mongodb=config["mongodb"]
 
 app=Flask(__name__,template_folder=f"{os.getcwd()}/templates")
 app.config['JSON_SORT_KEYS'] = False
-app.config["MONGO_URI"]=f"mongodb://{mongodb['HOST']}:{mongodb['PORT']}/{mongodb['DB']}"
-
+# app.config["MONGO_URI"]=f"mongodb://{mongodb['HOST']}:{mongodb['PORT']}/{mongodb['DB']}"
+app.config["MONGO_URI"]=f"mongodb+srv://zapDev:qVZWj5wVgct8ae1a@zapdev.yd03yyz.mongodb.net/{mongodb['DB']}"
 mongo=PyMongo(app)
